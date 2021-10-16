72543dc8dc88d9d00e431f3f862b72ac2ec1c951
liufanbiao <3381654532@qq.com>
2021-10-17 04:16:58 +0800

    hot-fix first commit

--- src/main/java/giTest.java
+++ src/main/java/giTest.java
@@ -4,5 +4,6 @@ public class giTest {
         System.out.println("hello git1");
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("hello git4");
     }
 }
