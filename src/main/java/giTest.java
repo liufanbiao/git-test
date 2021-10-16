55571fe18caa42b2bd7485c343a9ead87cfc0522
liufanbiao <3381654532@qq.com>
2021-10-17 03:48:45 +0800

    third commit

--- src/main/java/giTest.java
+++ src/main/java/giTest.java
@@ -3,5 +3,6 @@ public class giTest {
     public static void main(String[] args) {
         System.out.println("hello git1");
         System.out.println("hello git2");
+        System.out.println("hello git3");
     }
 }
