64694739040a0eab226cb32a5aeebe16f55bdc6e
liufanbiao <3381654532@qq.com>
2021-10-17 20:01:39 +0800

    master sixth commit

--- src/main/java/giTest.java
+++ src/main/java/giTest.java
@@ -6,7 +6,7 @@ public class giTest {
         System.out.println("hello git3");
         System.out.println("hello git4");
         System.out.println("hello git5");
-
         System.out.println("hello git5");
+        System.out.println("push code");
     }
 }
